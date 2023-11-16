f5d523e684016a3af92481dd55941824c5f513d7
perismuthoni <muthonimuriuki22@gmail.com>
2023-11-16 20:56:45 +0000

    progressive

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import LandingPage from './js/landing';
 import Navbar from './js/navbar';
 
-
 function App() {
-
-
   return (
     <Router>
       <Routes>
-      
-       
-
-       <Route path="/" element={<LandingPage />} />
-        <Route path="/navbar" element={<Navbar/>} /> 
-       
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/navbar" element={<Navbar />} />
       </Routes>
     </Router>
   );
